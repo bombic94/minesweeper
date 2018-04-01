5ee712f2526d555bc3c741cdc2a2bc38a43cdc50
bohmannd <bohmannd@gmail.com>
2018-04-01 18:52:07 +0200

    Happy scenario - can play game, if nothing goes wrong

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -26,10 +26,38 @@ namespace ConsoleApp
 
             oblast_id = CreateOrSelectGame(runningGames);
 
-            TiskInfo();
-            Console.Read();
+            Hrej(oblast_id); 
         }
-        
+
+        private static void Hrej(int oblast_id)
+        {
+            bool finished = false;
+            while (!finished)
+            {
+                TiskOblasti(oblast_id);
+                TiskInfo();
+                String response = Console.ReadLine();
+                String[] arr = response.Split();
+                int x = int.Parse(arr[1]);
+                int y = int.Parse(arr[2]);
+                switch (arr[0])
+                {
+                    case ("F"):
+                        OdkrytiPole(oblast_id, x, y);
+                        break;
+                    case ("M"):
+                        OznaceniMiny(oblast_id, x, y);
+                        break;
+                    case ("U"):
+                        ZruseniMiny(oblast_id, x, y);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong param");
+                        break;
+                }
+            }
+        }
+
         private static List<int> afterStart()
         {
             Console.WriteLine("Welcome to Minesweeper game");
@@ -135,24 +163,106 @@ namespace ConsoleApp
             Console.WriteLine("Mark new mine by instruction 'M [x] [y]");
             Console.WriteLine("Unmark mine by instruction 'U [x] [x]'");
         }
-        static int TiskOblasti(int obl_id)
+        static void TiskOblasti(int obl_id)
         {
-            return 0;
+            using (var db = new postgresEntities())
+            {
+                var listPoli = (from p in db.POLE
+                            where p.oblast == obl_id
+                            orderby p.souradnice_y, p.souradnice_x
+                            select p).ToList();
+                var listMin = (from m in db.MINA
+                            where m.oblast == obl_id
+                            select m).ToList();
+
+                Console.Write("    ");
+                for (int i = 0; i <= listPoli.Last().souradnice_x; i++)
+                {
+                    Console.Write(i.ToString("00") + " ");
+                }
+
+                foreach (var pole in listPoli)
+                {
+                    if (pole.souradnice_x == 0)
+                    {
+                        Console.Write("\n " + pole.souradnice_y.ToString("00") + " ");
+                    }
+                    bool oznacena_mina = false;
+                    foreach (var mina in listMin)
+                    {
+                        if (mina.souradnice_x == pole.souradnice_x && mina.souradnice_y == pole.souradnice_y)
+                        {
+                            Console.Write("?  ");
+                            oznacena_mina = true;
+                            break;
+                        }
+                    }
+                    if (!oznacena_mina) { 
+                        if (!pole.odkryto)
+                        {
+                            Console.Write("_  ");
+                        }
+                        else
+                        {
+                            if (pole.sousedni_miny == 0){
+                                Console.Write("X  ");
+                            }
+                            else
+                            {
+                                Console.Write(pole.sousedni_miny + "  ");
+                            }
+                        }
+                    }
+                }
+                Console.Write("\n ");
+            }
         }
 
-        static int OdkrytiPole(int x, int y, int obl_id)
+        static void OdkrytiPole(int obl_id, int x, int y)
         {
-            return 0;
+            using (var db = new postgresEntities())
+            {
+                var tah = new TAH
+                {
+                    oblast = obl_id,
+                    souradnice_x = x,
+                    souradnice_y = y                    
+                };
+
+                db.TAH.Add(tah);
+                db.SaveChanges();
+            }
         }
 
-        static int OznaceniMiny(int x, int y, int obl_id)
+        static void OznaceniMiny(int obl_id, int x, int y)
         {
-            return 0;
+            using (var db = new postgresEntities())
+            {
+                var mina = new MINA
+                {
+                    oblast = obl_id,
+                    souradnice_x = x,
+                    souradnice_y = y
+                };
+
+                db.MINA.Add(mina);
+                db.SaveChanges();
+            }
         }
 
-        static int ZruseniMiny(int x, int y, int obl_id)
+        static void ZruseniMiny(int obl_id, int x, int y)
         {
-            return 0;
+            using (var db = new postgresEntities())
+            {
+                var mina = (from m in db.MINA
+                           where m.oblast == obl_id
+                           where m.souradnice_x == x
+                           where m.souradnice_y == y
+                           select m).Single();
+
+                db.MINA.Remove(mina);
+                db.SaveChanges();
+            }
         }
     }
 }
