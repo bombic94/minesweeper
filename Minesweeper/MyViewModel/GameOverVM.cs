5292afc4c7d67ad68e88230b3747b115d6edc98d
bohmannd <bohmannd@gmail.com>
2018-04-29 13:38:06 +0200

    Implemented GameOver VM and Window. Edits in Continue Window

--- Minesweeper/MyViewModel/GameOverVM.cs
+++ Minesweeper/MyViewModel/GameOverVM.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Minesweeper.MyViewModel
 {
-    class GameOverVM
+    public class GameOverVM
     {
+        /// <summary>
+        /// Collection of lost games
+        /// </summary>
+        public ObservableCollection<HRA> LostGames { get; set; }
+
+        /// <summary>
+        /// Collection of won games
+        /// </summary>
+        public ObservableCollection<HRA> WonGames { get; set; }
+
+        public GameOverVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                LostGames = new ObservableCollection<HRA>();
+                WonGames = new ObservableCollection<HRA>();
+            }
+            else
+            {
+                Refresh();
+            }
+        }
+
+        private void Refresh()
+        {
+            LostGames = new ObservableCollection<HRA>(
+                DBHelper.LostGames()
+            );
+            WonGames = new ObservableCollection<HRA>(
+                DBHelper.WonGames()
+            );
+        }
     }
 }
