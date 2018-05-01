20cef5c920d0387998ae60722f4cc15f6710afd4
bohmannd <bohmannd@gmail.com>
2018-05-01 14:56:17 +0200

    Added canExecute for select in ContinueVM

--- Minesweeper/MyViewModel/ContinueVM.cs
+++ Minesweeper/MyViewModel/ContinueVM.cs
@@ -15,15 +15,19 @@ namespace Minesweeper.MyViewModel
     /// ViewModel for ContinueWindow
     /// Show table of games which are not finished and let player select one to continue
     /// </summary>
-    public class ContinueVM
+    public class ContinueVM : INotifyPropertyChanged
     {
+        /// <summary>
+        /// selected Game
+        /// </summary>
+        private HRA selected;
         /// <summary>
         /// Initializes a new instance of the <see cref="ContinueVM" /> class
         /// When window is opened, show all games which are not finished
         /// </summary>
         public ContinueVM()
         {
-            this.SelectCommand = new RelayCommand(param => this.SelectGame(param));
+            this.SelectCommand = new RelayCommand(param => this.SelectGame(), c => Selected != null);
 
             if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
             {
@@ -36,9 +40,30 @@ namespace Minesweeper.MyViewModel
         }
 
         /// <summary>
+        /// Property Changed event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
         /// Gets or sets collection of games which are not finished
         /// </summary>
         public ObservableCollection<HRA> Hry { get; set; }
+
+        /// <summary>
+        /// Gets or sets selected game to continue
+        /// </summary>
+        public HRA Selected
+        {
+            get
+            {
+                return selected;
+            }
+            set
+            {
+                selected = value;
+                OnPropertyChanged("Selected");
+            }
+        }
 
         /// <summary>
         /// Gets or sets command for Select
@@ -58,15 +83,24 @@ namespace Minesweeper.MyViewModel
         /// and close this window
         /// </summary>
         /// <param name="param">Selected game HRA</param>
-        private void SelectGame(object param)
+        private void SelectGame()
         {
-            HRA hra = (HRA)param;
+            HRA hra = Selected;
             int oblastID = (int)hra.oblast;
 
             MainVM main = Application.Current.MainWindow.DataContext as MainVM;
             main.OblastID = oblastID;
             main.GenerateGrid();
             Application.Current.Windows.OfType<ContinueWindow>().First().Close();
+        }
+
+        /// <summary>
+        /// OnPropertyChange handling
+        /// </summary>
+        /// <param name="name">Property to change</param>
+        protected void OnPropertyChanged(string name)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
